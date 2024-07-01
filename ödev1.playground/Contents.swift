dcb8aa1aa278e75033ac26fc94897c449220b12d
Ahmet Barut <ahmet0barut@gmail.com>
2024-07-01 18:50:32 +0300

    first commite swift

---
+++ ödev1.playground/Contents.swift
@@ -0,0 +1,98 @@
+import UIKit
+
+var ilce                = "manavgat"
+var kıta                = "avrupa"
+var faks                = "7000"
+var postaKodu           = "07600"
+var instaAdresi         = "theAhmetBarut"
+var calistigiBolum      = "Elektrik Elektronik Mühendisliği"
+var urunMiktari         = "7"
+var musteriSoyAdi       = "Barut"
+var ödemeMiktari        = "700"
+var dogumTarihi         = "27/03/2003"
+var borç                = "270"
+var medeniHali          = "Bekar"
+var videoYorumu         = "harika"
+var ödemeSaati          = "17"
+var eftMiktari          = "70000"
+var satilanMiktar       = "777"
+var telefonModeli       = "iphone 15"
+var dergiAdi            = "Adı Yok"
+var basimTarihi         = "13/05/2022"
+var zamMiktari          = "1270"
+var daireSayisi         = "6"
+var enlem               = "36.78"
+var boylam              = "31.44"
+var yemekAdi            = "Hamburger"
+var ürünFiyati          = "2400"
+var sirket              = "Barut's"
+var videoAdi            = "paralel evren"
+var müziksuresi         = "2"
+var mekanPuani          = "5"
+var dosyaAdi            = "bootcamp ödev"
+var resimFormati        = "jpeg"
+var renk                = "kahverengi"
+var renkKodu            = "964B00"
+var bilgisayarModeli    = "Macbook Air"
+var ekranBoyutu         = "13.6"
+var kullanimSuresi      = "24"
+var basinc              = "4"
+var etkinlikGunu        = "01/07/2024"
+var odemeGunu           = "01/07/2023"
+var yolculukCikisTarihi = "02/07/2024"
+var mahalleAdi          = "Bahcelievler"
+var otobusHatti         = "770"
+var kullanilanDakika    = "450"
+var kargoTakiphayir     = "54787"
+var kuponSuresi         = "1"
+var kuponKodu           = "8754362"
+var faturaTarihi        = "27/07/2022"
+
+
+
+print("ilçe:\(ilce)")
+print("kıta:\(kıta)")
+print("faks:\(faks)")
+print("posta kodu:\(postaKodu)")
+print("instagram adresi:\(instaAdresi)")
+print("çalıştığı bölüm:\(calistigiBolum)")
+print("ürün miktari:\(urunMiktari)")
+print("müsteri soyadı:\(musteriSoyAdi)")
+print("ödeme miktari:\(ödemeMiktari)")
+print("doğum tarihi:\(dogumTarihi)")
+print("borç:\(borç)")
+print("medeni hali:\(medeniHali)")
+print("video yorumu:\(videoYorumu)")
+print("ödeme saati:\(ödemeSaati)")
+print("eft miktari:\(eftMiktari)")
+print("satılan miktar:\(satilanMiktar)")
+print("telefon modeli:\(telefonModeli)")
+print("dergi adı:\(dergiAdi)")
+print("basım tarihi:\(basimTarihi)")
+print("zam miktarı:\(zamMiktari)")
+print("daire sayısı:\(daireSayisi)")
+print("enlem:\(enlem)")
+print("boylam:\(boylam)")
+print("yemek adı:\(yemekAdi)")
+print("şirket:\(sirket)")
+print("video adı:\(videoAdi)")
+print("müzik süresi:\(müziksuresi)")
+print("mekan puanı:\(mekanPuani)")
+print("dosya adı:\(dosyaAdi)")
+print("resim formatı:\(resimFormati)")
+print("renk:\(renk)")
+print("renk kodu:\(renkKodu)")
+print("bilgisayar modeli:\(bilgisayarModeli)")
+print("ekran boyutu:\(ekranBoyutu)")
+print("kullanım süresi:\(kullanimSuresi)")
+print("basınç:\(basinc)")
+print("etkinlik günü:\(etkinlikGunu)")
+print("ödeme günü:\(odemeGunu)")
+print("yolculuk çıkış tarihi:\(yolculukCikisTarihi)")
+print("mahalle adı:\(mahalleAdi)")
+print("otobüs hattı:\(otobusHatti)")
+print("kullanılan dakika:\(kullanilanDakika)")
+print("kargo takip no:\(kargoTakiphayir)")
+print("kupon süresi:\(kuponSuresi)")
+print("kupon kodu:\(kuponKodu)")
+print("fatura tarihi:\(faturaTarihi)")
